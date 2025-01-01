8cf7b8aa4566b27f273f530b2682b255ff557a0d
Emerson Millard <emillard.code@gmail.com>
2025-01-01 12:00:00 -0800

    Added additional Unit Testing for print methods.

--- src/test/java/com/project/MainTest.java
+++ src/test/java/com/project/MainTest.java
@@ -3,6 +3,26 @@ package com.project;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+
 public class MainTest {
+
+    @Test
+    public void testMain() {
+
+        assertTrue(true);
+
+    }
+
+    @Test
+    public void testPrint() throws Exception {
+
+        String text = tapSystemOut(() -> {
+            Main.print("Car");
+        });
+
+        assertEquals("Car", text.trim());
+
+    }
 
 }
